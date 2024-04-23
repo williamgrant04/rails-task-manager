b120ab442c1df29090096deb6d189c9f3b738401
William Grant <williamg3575467@gmail.com>
2024-04-23 10:43:49 +0100

    Refactor controller to use before_action and add link to new page (because i forgot)

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,10 +1,11 @@
 class TasksController < ApplicationController
+  before_action :get_task, except: [:index, :create, :new]
+
   def index
     @tasks = Task.all
   end
 
   def show
-    @task = Task.find(params[:id])
   end
 
   def new
@@ -21,26 +22,27 @@ class TasksController < ApplicationController
   end
 
   def edit
-    @task = Task.find(params[:id])
   end
 
   def update
-    @task = Task.find(params[:id])
-      if @task.update(task_params)
-        redirect_to @task
-      else
-        render 'edit'
-      end
+    if @task.update(task_params)
+      redirect_to @task
+    else
+      render 'edit'
+    end
   end
 
   def destroy
-    @task = Task.find(params[:id])
     @task.destroy
     redirect_to tasks_url
   end
 
 
   private
+
+  def get_task
+    @task = Task.find(params[:id])
+  end
 
   def task_params
     params.require(:task).permit(:title, :details, :completed) # Just in case
